adeaaa05e4c7cad52ed97385a23bdcc4c8a10663
Alejandro Mery <amery@jpi.io>
2023-03-25 19:23:23 +0000

    pkg/ca: NewTemplateFromCSR/NewCSRTemplate
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/ca/template.go
+++ pkg/ca/template.go
@@ -74,6 +74,36 @@ func (tc *TemplateConfig) NewCertificateTemplate(names ...string) *x509.Certific
 	return tpl
 }
 
+// NewTemplateFromCSR prepares a [x509.Certificate] from a [x509.CertificateRequest]
+func (tc *TemplateConfig) NewTemplateFromCSR(csr *x509.CertificateRequest) *x509.Certificate {
+	now := time.Now()
+	expiration := now.Add(tc.Duration)
+
+	tpl := &x509.Certificate{
+		SerialNumber:    RandomSerialNumber(),
+		Subject:         csr.Subject,
+		ExtraExtensions: csr.Extensions, // includes requested SANs, KUs and EKUs
+
+		NotBefore: now, NotAfter: expiration,
+
+		// If the CSR does not request a SAN extension, fix it up for them as
+		// the Common Name field does not work in modern browsers. Otherwise,
+		// this will get overridden.
+		DNSNames: []string{csr.Subject.CommonName},
+
+		// Likewise, if the CSR does not set KUs and EKUs, fix it up as Apple
+		// platforms require serverAuth for TLS.
+		KeyUsage:    x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+	}
+
+	if len(csr.EmailAddresses) > 0 {
+		tpl.ExtKeyUsage = append(tpl.ExtKeyUsage, x509.ExtKeyUsageEmailProtection)
+	}
+
+	return tpl
+}
+
 // NewCATemplate generates the template to create a new CA,
 // based on the information on the Issuer field.
 func (tc *TemplateConfig) NewCATemplate(skid []byte) *x509.Certificate {
@@ -98,6 +128,39 @@ func (tc *TemplateConfig) NewCATemplate(skid []byte) *x509.Certificate {
 		BasicConstraintsValid: true,
 		IsCA:                  true,
 		MaxPathLenZero:        true,
+	}
+
+	return tpl
+}
+
+// revive:disable:cognitive-complexity
+
+// NewCSRTemplate creates a Certificate Request Template for a list of names.
+// These names can be IP addresses, e-mail addresses, URIs or DNS names.
+func (tc *TemplateConfig) NewCSRTemplate(names ...string) *x509.CertificateRequest {
+	// revive:enable:cognitive-complexity
+	tpl := &x509.CertificateRequest{
+		Subject: pkix.Name{
+			Organization:       []string{tc.O},
+			OrganizationalUnit: []string{tc.OU},
+			CommonName:         tc.CN,
+		},
+	}
+
+	for _, h := range names {
+		if ip, ok := tc.asIPAddress(h); ok {
+			tpl.IPAddresses = append(tpl.IPAddresses, ip)
+		} else if s, ok := tc.asEmailAddress(h); ok {
+			tpl.EmailAddresses = append(tpl.EmailAddresses, s)
+		} else if u, ok := tc.asURI(h); ok {
+			tpl.URIs = append(tpl.URIs, u)
+		} else {
+			tpl.DNSNames = append(tpl.DNSNames, h)
+		}
+	}
+
+	if len(names) > 0 {
+		tpl.Subject.CommonName = names[0]
 	}
 
 	return tpl
