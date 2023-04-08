9549ea16c76fc15508d45540126d52a1388be870
Alejandro Mery <amery@jpi.io>
2023-04-08 01:28:11 +0100

    pkg/acme: add Identifier [§9.7.7]]
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ pkg/acme/identifier.go
@@ -0,0 +1,25 @@
+package acme
+
+// Identifier is an entity the account is
+// authorized to represent
+type Identifier struct {
+	// Type indicates the type of identifier
+	Type IdentifierType `json:"type"`
+	// Value indicates the identifier itself
+	Value string `json:"value"`
+}
+
+// IdentifierType indicates the type of identifier
+// that is present in ACME authorization objects
+// ([§9.7.7]).
+//
+// [§9.7.7]: https://www.rfc-editor.org/rfc/rfc8555#section-9.7.7
+type IdentifierType string
+
+const (
+	// IdentifierDNS defined by [RFC8555] for fully
+	// qualified domain names.
+	//
+	// [RFC8555]: https://www.rfc-editor.org/rfc/rfc8555
+	IdentifierDNS IdentifierType = "dns"
+)
