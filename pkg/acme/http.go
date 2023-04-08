6450377ee6e0b3fdd80e3ace8ac5604f133450ec
Alejandro Mery <amery@jpi.io>
2023-04-08 15:23:34 +0100

    pkg/acme: add ContentTypeDERCA for binary DER encoded CA certificates
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/acme/http.go
+++ pkg/acme/http.go
@@ -1,6 +1,10 @@
 package acme
 
 var (
+	// ContentTypeDERCA is the Content-Type used when rendering
+	// binary DER encoded CA Certificates
+	ContentTypeDERCA = "application/x-x509-ca-cert"
+
 	// ContentTypeJOSE is the ContentType used when rendering
 	// JWS as flattened JSON
 	ContentTypeJOSE = "application/jose+json"
