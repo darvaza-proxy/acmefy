e0192d301fb915e724953de9f96b91355b85d606
Alejandro Mery <amery@jpi.io>
2023-04-08 00:33:39 +0000

    pkg/acme: add Timestamp object
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/acme/acme.go
+++ pkg/acme/acme.go
@@ -2,3 +2,14 @@
 //
 // [RFC8555]: https://www.rfc-editor.org/rfc/rfc8555
 package acme
+
+import "time"
+
+// Timestamp renders in the format defined in [RFC3339]
+//
+// [RFC3399]: https://www.rfc-editor.org/rfc/rfc3339
+type Timestamp time.Time
+
+func (ts Timestamp) String() string {
+	return time.Time(ts).UTC().Format(time.RFC3339)
+}
