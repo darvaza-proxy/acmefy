f8bee68563f443e29d72a4443b7bec86f727d3cf
Alejandro Mery <amery@jpi.io>
2023-04-08 14:14:00 +0000

    pkg/acme: add ContentTypePEMCertChain for PEM Certificate chains
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/acme/http.go
+++ pkg/acme/http.go
@@ -1,6 +1,10 @@
 package acme
 
 var (
+	// ContentTypePEMCertChain is the ContentType used when rendering
+	// PEM Certificate chains
+	ContentTypePEMCertChain = "application/pem-certificate-chain"
+
 	// ContentTypeProblem is the ContentType used when rendering a Problem
 	ContentTypeProblem = "application/problem+json"
 )
