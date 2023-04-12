cec0f3447d54e2852f45aae2ff4e3f672428b04b
Alejandro Mery <amery@jpi.io>
2023-04-12 17:24:04 +0000

    pkg/magic: introduce Magic ACME client
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ pkg/magic/magic.go
@@ -0,0 +1,19 @@
+// Package magic provides a ACME client implementation
+package magic
+
+import (
+	"sync"
+
+	"darvaza.org/resolver"
+	"darvaza.org/slog"
+)
+
+// Magic is an ACME client that attempts to
+// simplify usage for dns and ip authorizations
+type Magic struct {
+	mu       sync.Mutex
+	logger   slog.Logger
+	resolver resolver.Resolver
+
+	entrypoint string
+}
