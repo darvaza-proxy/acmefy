40bd9214f238729921feab88f4185b4a4a054c1f
Alejandro Mery <amery@jpi.io>
2023-04-12 17:46:48 +0000

    pkg/magic: add setTLSALPN01Remote()
    initial support for an address to be used when forwarding
    TLS-ALPN-01 requests to the ACME server
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/magic/setup.go
+++ pkg/magic/setup.go
@@ -61,3 +61,19 @@ func (m *Magic) SetResolver(dns resolver.Resolver) error {
 func defaultResolver() resolver.Resolver {
 	return resolver.SystemResolver(true)
 }
+
+// SetTLSALPN01Remote sets the optional address intended
+// to be used when forwarding TLS-ALPN-01 requests to
+// a dedicated ACME server to handle the authorization
+func (m *Magic) SetTLSALPN01Remote(remote string) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	_, _, err := core.SplitHostPort(remote)
+	if err != nil {
+		return core.Wrap(err, "acmefy/magic: SetTLSALPN01Remote")
+	}
+
+	m.forwardTLSALPN01 = remote
+	return nil
+}
