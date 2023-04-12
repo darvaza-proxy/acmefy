886ed2c0a0354c799013f38e876bc9760c468b1c
Alejandro Mery <amery@jpi.io>
2023-04-12 16:21:27 +0000

    pkg/client: Store and Client placeholder
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,2 +1,19 @@
 // Package client provides generic interfaces and helpers to implement ACME clients
 package client
+
+import "darvaza.org/acmefy/pkg/acme"
+
+var (
+	_ acme.Client = (*Client)(nil)
+)
+
+// Client represents the connection to a particular ACME server
+type Client struct {
+	directory acme.Directory
+}
+
+// Directory tells the different endpoints provided by a
+// particular ACME Server
+func (c Client) Directory() acme.Directory {
+	return c.directory
+}
--- pkg/client/go.mod
+++ pkg/client/go.mod
@@ -2,4 +2,10 @@ module darvaza.org/acmefy/pkg/client
 
 go 1.22
 
+toolchain go1.22.6
+
+require darvaza.org/acmefy v0.4.5
+
+require github.com/go-jose/go-jose/v4 v4.0.5 // indirect
+
 replace darvaza.org/acmefy => ../../
