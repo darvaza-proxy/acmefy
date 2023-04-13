08f27836787c279758e5f9c38562af476d90478b
Alejandro Mery <amery@jpi.io>
2023-04-13 14:03:40 +0000

    pkg/magic: add log level shortcuts
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ pkg/magic/logger.go
@@ -0,0 +1,33 @@
+package magic
+
+// func (m *Magic) error(err error) slog.Logger {
+// 	l := m.logger.Error()
+// 	if err != nil {
+// 		l = l.WithField(slog.ErrorFieldName, err)
+// 	}
+// 	return l
+// }
+
+// func (m *Magic) warn(err error) slog.Logger {
+// 	l := m.logger.Warn()
+// 	if err != nil {
+// 		l = l.WithField(slog.ErrorFieldName, err)
+// 	}
+// 	return l
+// }
+
+// func (m *Magic) info() slog.Logger {
+// 	return m.logger.Info()
+// }
+
+// func (m *Magic) debug() slog.Logger {
+// 	return m.logger.Debug()
+// }
+
+// func (m *Magic) withInfo() (slog.Logger, bool) {
+// 	return m.logger.Info().WithEnabled()
+// }
+
+// func (m *Magic) withDebug() (slog.Logger, bool) {
+// 	return m.logger.Debug().WithEnabled()
+// }
